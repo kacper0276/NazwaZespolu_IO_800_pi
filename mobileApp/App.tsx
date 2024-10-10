ad2ee292664ce1a14f590a14f082b288c5faaba6
kacper0276 <kacper0276@op.pl>
2024-10-10 15:27:06 +0200

    [MB] Create PopUp component

--- mobileApp/App.tsx
+++ mobileApp/App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import PopUp from "./components/PopUp";
 
 export default function App() {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const showModalFunc = () => {
+    setShowModal(true);
+  };
+
+  const closeModalFunc = () => {
+    setShowModal(false);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={showModalFunc}>
+        <Text>KLIK</Text>
+      </TouchableOpacity>
+      {showModal && (
+        <PopUp
+          body={<Text>BODY</Text>}
+          header={<Text>Header</Text>}
+          closeModal={closeModalFunc}
+        />
+      )}
     </View>
   );
 }
@@ -13,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ mobileApp/components/PopUp.tsx
@@ -0,0 +1,57 @@
+import React, { FC } from "react";
+import { Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+type Props = {
+  header: React.ReactNode;
+  body: React.ReactNode;
+  closeModal: () => void;
+};
+
+const PopUp: FC<Props> = ({ header, body, closeModal }) => {
+  return (
+    <Modal animationType="slide" transparent={true} visible={true}>
+      <View style={styles.backdrop}>
+        <View style={styles.mainContainer}>
+          <View style={styles.header}>
+            {header}
+            <TouchableOpacity onPress={closeModal}>
+              <Text style={styles.closeButton}>X</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.body}>{body}</View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  backdrop: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  mainContainer: {
+    width: "80%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  body: {
+    width: "100%",
+  },
+  closeButton: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default PopUp;
