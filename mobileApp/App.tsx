96f8be83587927658e00c142c458b2cb8f68b2c3
kacper0276 <kacper0276@op.pl>
2024-10-23 13:38:44 +0200

    [MB] Use context in App.tsx

--- mobileApp/App.tsx
+++ mobileApp/App.tsx
@@ -4,6 +4,7 @@ import { RootStackParamList } from "./types/navigation.types";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import DetailsScreen from "./screens/DetailsScreen";
+import { ScreenRotateProvider } from "./context/screenRotateContext";
 
 export const API_URL = "http://localhost:3001";
 
@@ -11,22 +12,24 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Strona Główna" }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          options={{ title: "Szczegóły" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ScreenRotateProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: "Strona Główna" }}
+          />
+          <Stack.Screen
+            name="Details"
+            component={DetailsScreen}
+            options={{ title: "Szczegóły" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ScreenRotateProvider>
   );
 }
