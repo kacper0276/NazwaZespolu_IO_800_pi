8d5249eab6aa6f588e59b94c3a8d43a8e2953f80
kacper0276 <kacper0276@op.pl>
2024-11-07 22:18:06 +0100

    [MB] Add icon in settings screen

--- mobileApp/screens/SettingsScreen.tsx
+++ mobileApp/screens/SettingsScreen.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { View, Text, Switch, StyleSheet } from "react-native";
+import { View, Text, Switch, StyleSheet, TouchableOpacity } from "react-native";
 import { useSettingsContext } from "../context/settingsContext";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { RootStackParamList } from "../types/navigation.types";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-const SettingsScreen: React.FC = () => {
+type SettingsScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "Settings"
+>;
+
+type SettingsScreenProps = {
+  navigation: SettingsScreenNavigationProp;
+};
+
+const SettingsScreen: React.FC<SettingsScreenProps> = ({ navigation }) => {
   const { theme, toggleTheme } = useSettingsContext();
   const isDarkMode = theme === "dark";
 
@@ -13,6 +25,17 @@ const SettingsScreen: React.FC = () => {
         { backgroundColor: isDarkMode ? "#000" : "#fff" },
       ]}
     >
+      <TouchableOpacity
+        onPress={() => navigation.goBack()}
+        style={styles.backButton}
+      >
+        <Icon
+          name="arrow-back"
+          size={30}
+          color={isDarkMode ? "#fff" : "#000"}
+        />
+      </TouchableOpacity>
+
       <Text style={[styles.title, { color: isDarkMode ? "#fff" : "#000" }]}>
         Ustawienia
       </Text>
@@ -29,6 +52,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    position: "relative",
   },
   title: {
     fontSize: 24,
@@ -40,6 +64,12 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     width: "80%",
     padding: 10,
+  },
+  backButton: {
+    position: "absolute",
+    left: 15,
+    top: 15,
+    zIndex: 1,
   },
 });
 
