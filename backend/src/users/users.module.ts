84ff0e37bdc884a17d5c1a834dde0f45fb6b8f8b
kacper0276 <kacper0276@op.pl>
2024-11-23 13:54:36 +0100

    [BE] Create users module

---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './entities/user.entity';
+import { UsersRepository } from './users.repository';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UsersService, UsersRepository],
+  controllers: [UsersController],
+})
+export class UsersModule {}
