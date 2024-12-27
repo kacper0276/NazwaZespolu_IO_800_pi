6ca77fe3646788db1bd4be12977f72316471bc15
kacper0276 <kacper0276@op.pl>
2024-12-27 15:15:12 +0100

    [FE] Fix to save sessions after refresh page

--- frontend/src/hoc/AuthenticatedRoute.tsx
+++ frontend/src/hoc/AuthenticatedRoute.tsx
@@ -1,15 +1,29 @@
 import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useUser } from "../context/UserContext";
+import LocalStorageService from "../services/localStorage.service";
+import { UserType } from "../types/IUser";
 
 const AuthenticatedRoute: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const userData = useUser().user;
+  const userContext = useUser();
 
-  if (userData) {
+  if (userContext.user && userContext.token && userContext.refreshToken) {
     return <>{children}</>;
   } else {
+    const storedUser = LocalStorageService.getItem<UserType>("user");
+    const storedAccessToken =
+      LocalStorageService.getItem<string>("accessToken");
+    const storedRefreshToken =
+      LocalStorageService.getItem<string>("refreshToken");
+
+    if (storedUser && storedAccessToken && storedRefreshToken) {
+      userContext.login(storedUser, storedAccessToken, storedRefreshToken);
+
+      return <>{children}</>;
+    }
+
     return <Navigate to={"/welcome-page"} />;
   }
 };
