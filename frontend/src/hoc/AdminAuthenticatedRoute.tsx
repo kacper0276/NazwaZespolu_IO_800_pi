9a034ad8fdece89097bacef1351c37e8b3c16f1c
kacper0276 <kacper0276@op.pl>
2024-12-28 00:18:10 +0100

    [FE] Update in AdminAuthRoute

--- frontend/src/hoc/AdminAuthenticatedRoute.tsx
+++ frontend/src/hoc/AdminAuthenticatedRoute.tsx
@@ -1,15 +1,34 @@
 import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useUser } from "../context/UserContext";
+import LocalStorageService from "../services/localStorage.service";
+import { UserType } from "../types/IUser";
 
 const AuthenticatedAdminRoute: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const userData = useUser().user;
+  const userContext = useUser();
 
-  if (userData?.role === "admin") {
+  if (
+    userContext.user &&
+    userContext.token &&
+    userContext.refreshToken &&
+    userContext.user?.role === "admin"
+  ) {
     return <>{children}</>;
   } else {
+    const storedUser = LocalStorageService.getItem<UserType>("user");
+    const storedAccessToken =
+      LocalStorageService.getItem<string>("accessToken");
+    const storedRefreshToken =
+      LocalStorageService.getItem<string>("refreshToken");
+
+    if (storedUser && storedAccessToken && storedRefreshToken) {
+      userContext.login(storedUser, storedAccessToken, storedRefreshToken);
+
+      return <>{children}</>;
+    }
+
     return <Navigate to={"/welcome-page"} />;
   }
 };
