7d509e427c94eb996d79e8a6109f926f8d10b515
Tomasz2002 <121009782+Tomasz2002@users.noreply.github.com>
2024-12-28 18:31:38 +0100

    Adding ChallengeCreation form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import ChallengeCreation from "./pages/ChallengeCreation/ChallengeCreation"
 import MainPage from "./pages/MainPage";
 import Sidebar from "./layout/Sidebar/Sidebar";
 import { UserProvider } from "./context/UserContext";
@@ -37,6 +38,14 @@ const content = (
         element={
           <NoAuthenticatedRoute>
             <WelcomePage />
+          </NoAuthenticatedRoute>
+        }
+      />
+            <Route
+        path="/create-challenge"
+        element={
+          <NoAuthenticatedRoute>
+            <ChallengeCreation/>
           </NoAuthenticatedRoute>
         }
       />
