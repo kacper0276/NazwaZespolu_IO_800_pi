3059dd9fadcc32c26e238520591fa5621380f059
kacper0276 <kacper0276@op.pl>
2024-12-29 20:35:35 +0100

    [FE] Check if device is mobile or tablet

--- frontend/src/pages/Settings/Settings.tsx
+++ frontend/src/pages/Settings/Settings.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import Spinner from "../../components/Spinner/Spinner";
+import useWebsiteTitle from "../../hooks/useWebsiteTitle";
+import { useTranslation } from "react-i18next";
+import styles from "./Settings.module.scss";
 
 const Settings: React.FC = () => {
+  const { t } = useTranslation();
+  useWebsiteTitle(t("settings"));
+
   return (
-    <div>
-      <p>dasddd</p>
-      <Spinner />
+    <div className={styles.mainContainer}>
+      <h1>Ustawienia</h1>
     </div>
   );
 };
