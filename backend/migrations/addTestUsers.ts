f4971d0fda09a1dc8d31ed130419d58c8febee70
kacper0276 <kacper0276@op.pl>
2025-01-09 23:14:32 +0100

    [BE] Create profile for user

--- backend/migrations/addTestUsers.ts
+++ backend/migrations/addTestUsers.ts
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import { UserSchema } from '../src/users/entities/user.entity';
+import { ProfileSchema } from '../src/profiles/entities/profile.entity';
 import { Role } from '../src/enums/role.enum';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-async function createTestUsers() {
+async function createTestUsersAndProfiles() {
   const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/';
 
   await mongoose.connect(mongoUri, {
@@ -15,6 +16,7 @@ async function createTestUsers() {
   });
 
   const User = mongoose.model('User', UserSchema);
+  const Profile = mongoose.model('Profile', ProfileSchema);
 
   const numberOfUsers = 10;
   const saltOrRounds = 10;
@@ -24,29 +26,52 @@ async function createTestUsers() {
     const plainPassword = `test123${i}`;
     const hashedPassword = await bcrypt.hash(plainPassword, saltOrRounds);
 
-    const existingUser = await User.findOne({ email: testEmail });
-    if (existingUser) {
+    let user = await User.findOne({ email: testEmail });
+    if (!user) {
+      user = new User({
+        email: testEmail,
+        password: hashedPassword,
+        role: i % 2 === 0 ? Role.ADMIN : Role.USER,
+        isActivated: true,
+      });
+
+      user = await user.save();
+      console.log(
+        `Utworzono użytkownika: ${testEmail}, hasło: ${plainPassword}`,
+      );
+    } else {
       console.log(`Użytkownik ${testEmail} już istnieje.`);
-      continue;
     }
 
-    const newUser = new User({
-      email: testEmail,
-      password: hashedPassword,
-      role: i % 2 === 0 ? Role.ADMIN : Role.USER,
-      isActivated: true,
-      profileId: '',
+    const existingProfile = await Profile.findOne({
+      userId: user._id.toString(),
     });
+    if (!existingProfile) {
+      const newProfile = new Profile({
+        currentGoals: [`Cel użytkownika ${i}`],
+        completedGoals: [],
+        followers: [],
+        following: [],
+        premium: i % 2 === 0,
+        posts: [],
+        userId: user._id.toString(),
+        user: user._id,
+      });
 
-    await newUser.save();
-    console.log(`Utworzono użytkownika: ${testEmail}, hasło: ${plainPassword}`);
+      await newProfile.save();
+      console.log(
+        `Utworzono profil dla użytkownika ${testEmail}, ID profilu: ${newProfile._id}`,
+      );
+    } else {
+      console.log(`Profil dla użytkownika ${testEmail} już istnieje.`);
+    }
   }
 
   await mongoose.disconnect();
-  console.log('Tworzenie użytkowników zakończone.');
+  console.log('Tworzenie użytkowników i profili zakończone.');
 }
 
-createTestUsers().catch((err) => {
+createTestUsersAndProfiles().catch((err) => {
   console.error('Wystąpił błąd:', err);
   process.exit(1);
 });
