488e89cbc6fdd4c6cf35a56577e3280992a9972c
kacper0276 <kacper0276@op.pl>
2025-01-10 18:01:34 +0100

    [FE] Create method to conver date

---
+++ frontend/src/helpers/convertDate.ts
@@ -0,0 +1,20 @@
+export function convertIsoToLocal(
+  isoString: string,
+  locale: string = "pl-PL"
+): string {
+  const date = new Date(isoString);
+
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
+
+  const formattedDate = new Intl.DateTimeFormat(locale, options).format(date);
+
+  return formattedDate;
+}
