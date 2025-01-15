0edef2ea02c673871af75257f3d5a03713e7ee6e
kacper0276 <kacper0276@op.pl>
2025-01-15 12:37:31 +0100

    [FE] Display only date without time

--- frontend/src/helpers/convertDate.ts
+++ frontend/src/helpers/convertDate.ts
@@ -8,9 +8,9 @@ export function convertIsoToLocal(
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
+    // hour: "2-digit",
+    // minute: "2-digit",
+    // second: "2-digit",
     hour12: false,
   };
 
